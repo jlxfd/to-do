5b46be38eeac0d5f300c9691ec411da05dcedf23
jlxfd <julialexisfd@gmail.com>
2023-12-29 01:49:48 +0800

    added product model and CRUD functions

--- server.js
+++ server.js
@@ -1,12 +1,79 @@
 const express = require('express');
+const Product = require('./models/productModel')
+const mongoose = require('mongoose');
 const app = express();
 
 app.use(express.json())
+app.use(express.urlencoded({extended: false}))
 
 app.get('/', (req,res) => {
     res.send(('Hello NODE API'))
 })
 
-app.listen(3000, () => {
-    console.log('To Do App is running on port 3000')
+
+app.get('/todo-entries', async(req, res) => {
+    try {
+        const todoEntries = await Product.find({})
+        res.status(200).json(todoEntries)
+    } catch (error) {
+        console.log(500).json({message: error.message})
+    }
+})
+
+app.get('/todo-entries/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const todoEntry = await Product.findById(id)
+        res.status(200).json(todoEntry)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.put('/todo-entries/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const todoEntry = await Product.findByIdAndUpdate(id, req.body)
+        if (!todoEntry){
+            return res.status(404).json({message: `cannot find todo entry with id ${id}`})
+        }
+        const updatedTodoEntry = await Product.findById(id)
+        res.status(200).json(updatedTodoEntry)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+app.post('/todo-entries', async(req, res) => {
+    try {
+        const todoEntry = await Product.create(req.body)
+        res.status(200).json(todoEntry)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+        console.log(500).json({message: error.message})
+    }
+})
+
+app.delete('/todo-entries/:id', async(req, res) => {
+    try {
+        const {id} = req.params
+        const todoEntry = await Product.findByIdAndDelete(id)
+        if (!todoEntry){
+            return res.status(404).json({message: `cannot find todo entry with id ${id}`})
+        }
+        res.status(200).json(todoEntry)
+    } catch (error) {
+        res.status(500).json({message: error.message})
+    }
+})
+
+mongoose.
+connect('mongodb+srv://admin:fCI4bQtest@cluster0.pnxdbkq.mongodb.net/ToDoApp?retryWrites=true&w=majority')
+.then(() => {
+    app.listen(3000, () => {
+        console.log('To Do App is running on port 3000')
+    })
+    console.log('connected to mongoDB')
+}).catch((error) => {
+    console.log(error)
 })
